65c46ee25fee4dba34325eb9991396077df42168
Dhruv Desai <dde@narola.email>
2019-08-08 19:03:25 +0530

    made it better

--- app.py
+++ app.py
@@ -15,21 +15,3 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Dashboard - Cryptoresearch'
 server = app.server
 app.config.suppress_callback_exceptions = True
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=True),
-    html.Div(id='page-content')
-])
-
-# Callback for routing
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/':
-        return cat_tabs
-    elif pathname == '/graph':
-        return graph.load_main_chart()
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
---
+++ index.py
@@ -0,0 +1,31 @@
+'''
+This module handles routing of different pages in the dashboard and
+is responsible for rendering correct modules as per user interaction
+'''
+import plotly.graph_objs as go
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from graph import load_main_chart
+from app import app
+from cat_page import cat_tabs
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=True),
+    html.Div(id='page-content')
+])
+
+# Callback for routing
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        return cat_tabs
+    elif pathname == '/graph':
+        return load_main_chart()
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
